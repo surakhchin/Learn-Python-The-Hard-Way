779e6f1e10a1b8b8247fd18ed64e6c22c3bb9120
Serge Urakhchin <urakhchin@gmail.com>
2016-03-24 00:09:43 -0500

    ex38, lists, and how python uses them
    stuff.append("hello") reads as append(stuff, "hello")

--- ex38.py
+++ ex38.py
@@ -1,8 +1,9 @@
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
-
+print ten_things
 print "Wait there are not 10 things in that list. Let's fix that."
 
 stuff = ten_things.split(' ')
+print stuff
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:
