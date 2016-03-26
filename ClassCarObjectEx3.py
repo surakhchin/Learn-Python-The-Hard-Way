24641916176e5f1deefbca25c6bb5011f5419161
Serge Urakhchin <urakhchin@gmail.com>
2016-03-26 02:55:48 -0500

    Playing around with extraction

--- ClassCarObjectEx3.py
+++ ClassCarObjectEx3.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
+
 class Vehicle(object):
     """ A vehicle for sale by Serge U. Dealership.
         Attributes:
@@ -15,9 +16,8 @@ class Vehicle(object):
 
     base_sale_price = 0
 
-    def __init__(self, wheels, miles, make, model, year, sold_on):
+    def __init__(self, miles, make, model, year, sold_on):
         """
-        :param wheels:
         :param miles:
         :param make:
         :param model:
@@ -25,7 +25,6 @@ class Vehicle(object):
         :param sold_on:
         :return: Return new Vehicle object.
         """
-        self.wheels = wheels
         self.miles = miles
         self.make = make
         self.model = model
@@ -36,17 +35,25 @@ class Vehicle(object):
         """
         :return: Return the sale price for the vehicle as a float amount.
         """
+
         if self.sold_on is not None:
-            return 0.00 # Already sold
-        return 5000.0 * self.wheels
+            return 0.00  # Already sold
+        return self.base_sale_price * self.numberofwheels()
 
     def purchase_price(self):
         """
         :return Return the price for which we would pay to purchase the vehicle.
         """
         if self.sold_on is None:
-            return 0.0 # Not yet sold
+            return 0.0  # Not yet sold
         return self.base_sale_price - (.10 * self.miles)
+
+    @abstractmethod
+    def numberofwheels(self):
+        """
+        :return: Return an integer representing number of wheels
+        """
+        pass
 
     @abstractmethod
     def vehicle_type(self):
@@ -62,7 +69,12 @@ class Car(Vehicle):
     """
 
     base_sale_price = 8000
-    wheels = 4
+
+    def numberofwheels(self):
+        """
+        :return: Integer w/ number of wheels
+        """
+        return 4
 
     def vehicle_type(self):
         """
@@ -77,7 +89,12 @@ class Truck(Vehicle):
     """
 
     base_sale_price = 10000
-    wheels = 4
+
+    def numberofwheels(self):
+        """
+        :return: Integer w/ number of wheels
+        """
+        return 4
 
     def vehicle_type(self):
         """
@@ -92,7 +109,12 @@ class Motorcycle(Vehicle):
     """
 
     base_sale_price = 4000
-    wheels = 2
+
+    def numberofwheels(self):
+        """
+        :return: Integer w/ number of wheels
+        """
+        return 2
 
     def vehicle_type(self):
         """
@@ -101,5 +123,10 @@ class Motorcycle(Vehicle):
         return 'motorcycle'
 
 
-bike = Vehicle(4, 40000, "Nissan", "Leaf", 2015, None)
+bike = Motorcycle(40000, "Kawasaki", "Ninja", 2015, None)
+print bike.vehicle_type()
+print bike.numberofwheels()
+print bike.sale_price()
 
+car = Car(11000,"Nissan", "Leaf", 2016, None)
+print car.sale_price()
